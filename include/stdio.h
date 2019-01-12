758db2ac1cfa248fffca7f796f29090eb34875c9
luojia65 <me@luojia.cc>
2019-01-12 17:04:35 +0800

    save

--- include/stdio.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STDIO_H_
-#define _STDIO_H_
-
-// extern int scanf(const char *format, ...);
-
-// extern int printf(const char *format, ...);
-
-#endif /* _STDIO_H_ */
--- librcc-lexer/src/lib.rs
+++ librcc-lexer/src/lib.rs
@@ -1,10 +1,3 @@
-#![feature(euclidean_division)]
-
-type RccDouble = f64;
-type RccInt = i32;
-
-mod math;
-
 #[cfg(test)]
 mod tests {
     #[test]
